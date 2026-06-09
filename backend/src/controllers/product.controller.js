297938caa675b3caa420fb537ac370223ae17b56
PRUTHVIRAJ SOLANKI <parthsolanki3337@gmail.com>
2026-06-09 12:25:38 +0530

    Update product.controller.js

--- backend/src/controllers/product.controller.js
+++ backend/src/controllers/product.controller.js
@@ -1,31 +1,70 @@
 import productModel from "../models/product.model.js";
 
-export const getProduct = async (req, res) => {
-
-    // const products = await productModel.find()
-    // return res.status(200).json({
-    //     success: true,
-    //     message: "All product fetch succesull",
-    //     data: products
-    // })
+export const getAllProduct = async (req, res) => {
 
     try {
-        const products = await productModel.find();
+        const products = await productModel.find().sort({ createdAt: 1 })
         return res.status(200).json({
             success: true,
-            message: "All product fetch successful",
+            message: "All product fetch succesull",
             data: products
-        });
+        })
     } catch (error) {
-        console.log("Error fetching products:", error.message);
-        
-        // This stops Vercel from sending the HTML crash page!
         return res.status(500).json({
             success: false,
-            message: "Failed to fetch products from the database",
-            error: error.message
-        });
+            message: "Internal server error",
+            error: error.message,
+            data: null
+        })
     }
+}
+
+export const getProductDetails = async (req, res) => {
+
+    const _id = req.params.id;
+    if (!_id) {
+        return res.status(400).json({
+            success: false,
+            message: "products id is not existing",
+            data: null
+        })
+    }
+    try {
+
+        const product = await productModel.findOne({ _id: _id })
+        if (!product) {
+            return res.status(404).json({
+                success: false,
+                message: "products not found",
+                data: null
+            })
+        }
+
+        return res.status(200).json({
+            success: true,
+            message: "Product detailed fetch ",
+            data: product,
+        });
+    } catch (error) {
+
+        if (error instanceof Error) {
+            return res.status(404).json({
+                success: false,
+                message: "Product not found",
+                data: null,
+                error: error.message,
+            })
+        } else {
+
+            return res.status(500).json({
+                success: false,
+                message: "Internal server error",
+                data: null,
+                error: error.message,
+            })
+        }
+    }
+
 }
 
 export const createProduct = async (req, res) => {
@@ -60,3 +99,43 @@ export const createProduct = async (req, res) => {
         })
     }
 }
+
+export const updateProduct = async (req, res) => {
+    const _id = req.params.id;
+    const { name, price, image, category } = req.body;
+    console.log(name);
+    if (!_id) {
+        return res.status(400).json({
+            success: true,
+            message: "Product id not Exist",
+            data: null
+        })
+    }
+
+    try {
+
+        const update = await productModel.findByIdAndUpdate(
+            { _id: _id },
+            {
+                $set: {
+                    name: name,
+                    price: price,
+                    image: image,
+                    category: category,
+                }
+            },
+            { new: true }
+        )
+
+        res.status(201).json({ update })
+
+
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: "Internal server error..." + error.message,
+            data: null
+        })
+    }
+}
+
